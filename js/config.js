6766249a09aa928382140d151b7aafd653a7c2a3
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2024-08-18 16:44:32 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,16 +4,16 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0x75Bf5634F463C3145a90897118b3C08FFA284a95";
+var FeeReciever = "0x5fa82AaEEf12C0f983f521e415a5e72F0AdB2291";
 
 
-var BinanceFee=0.01;
-var PolygonFee=0.1;
-var FantomFee=0.12;
-var RopstenFee=0.001;
+var BinanceFee=0.02;
+var PolygonFee=0.2;
+var FantomFee=0.14;
+var RopstenFee=0.0010;
 var HuobiFee=0.01;
-var AvalancheFee=0.1;
-var EthereumFee=0.01;
+var AvalancheFee=0.2;
+var EthereumFee=0.02;
 var CronosFee=3;
 var PhoenixFee=0.1;
 var CandleFee=1;
